5f6bf74d93d0d6d3d9311567a9370ce5ed6eebb7
majia2968 <majia3080@gmail.com>
2015-06-04 21:46:03 -0400

    Change Middle number test

--- src/main/test/com/myjava/pro/MiddleNumberTest.java
+++ src/main/test/com/myjava/pro/MiddleNumberTest.java
@@ -1,29 +1,38 @@
 package com.myjava.pro;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
+import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 public class MiddleNumberTest {
+	private MiddleNumber mn;
+	private int[] arr;
+	private int[] arr1;
+	
+	@Before
+	public void setUp() {
+		mn = new MiddleNumber();
+		arr = new int[]{ 1, 3, 5, 7, 8, 8 };
+		arr1 = new int[]{ 1, 3, 5, 7, 8, 5 };
+	}
 
 	@Test
 	public void test() throws Exception {
-		int[] arr = {1,3,5,7,8,8};
-	
-		MiddleNumber mn = new MiddleNumber();
 		assertThat(mn.findMiddleNumber(arr), is(3));
 	}
-	
+
 	@Test
 	public void testException() throws Exception {
-		int[] arr = {1,3,5,7,8,5};	
-		MiddleNumber mn = new MiddleNumber();
-		
-	    try {
+
+		try {
 			assertThat(mn.findMiddleNumber(arr), is(3));
-	    } catch (Exception e) {
-	        assertThat("Please pass proper array to match the requirement", is(e.getMessage()));
-	    }
+		} catch (Exception e) {
+			assertThat("Please pass proper array to match the requirement",
+					is(e.getMessage()));
+		}
 
 	}
 
